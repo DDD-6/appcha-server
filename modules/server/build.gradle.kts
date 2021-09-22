8b827907a531e14c9aba3cc711a34409a2a905e6
HongJow <hoongjow@gmail.com>
2021-09-22 23:52:11 +0900

    Initialize helm

--- modules/server/build.gradle.kts
+++ modules/server/build.gradle.kts
@@ -1,8 +1,16 @@
 
+plugins {
+    id("com.palantir.docker") version "0.26.0"
+}
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
 
     api("org.springframework.boot:spring-boot-starter-test")
+}
+
+docker {
+    name = "appcha-server"
 }
